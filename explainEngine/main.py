031bd87495950fdc2071c9286057e3e988c6d78e
xudaaaaan <xudaaaaan@163.com>
2019-06-01 20:48:05 -0700

    added GRU explainer

---
+++ explainEngine/main.py
@@ -0,0 +1,94 @@
+import util.yelp as yelp
+import util.sentiment as sentiment
+from hnatt import HNATT
+
+YELP_DATA_PATH = '../data/yelp/review.json'
+SENTIMENT_DATA_PATH = '../data/sentiment/sentiment.tar.gz'
+SENT_SAVED_MODEL_DIR = 'saved_models/sentiment/'
+YELP_SAVED_MODEL_DIR  = 'saved_models/yelp/'
+SAVED_MODEL_FILENAME = 'model.h5'
+
+def find_confidence_cases(h, dev_x, dev_y, threshold):
+	"""
+
+	:param h: pretrained HNATT model
+	:param dev_x: np.array. dev data set
+	:param dev_y: np.array. dev label set.
+	:param threshold: determine confidence
+	:return: list[sentence]
+	"""
+	with open("../data/sentiment/overconfident.txt", 'w') as over_confident_file:
+		with open("../data/sentiment/confident.txt", 'w') as confident_file:
+			cases = list(zip(dev_x, dev_y))
+			for text, label in cases:
+				preds = h.predict([text])[0]
+				if max(preds) >= threshold:
+					pass
+	pass
+
+
+
+
+
+def visual_info(h, testcase, label = ""):
+	"""
+
+	:param h: pretrained HNATT model
+	:param testcase: a review you want to test and visualize
+	:return: dict{sentence, predicted_label, ground_truth, confidence, word_weights}
+
+	"""
+	dic = {}
+	dic['sentence'] = testcase
+	preds = h.predict([testcase])
+	dic['probs'] = preds[0]
+	if label:
+		dic["ground_truth"] = label
+	else:
+		dic["ground_truth"] = "NOT GIVEN"
+
+	activation_maps = h.activation_maps(testcase)
+	sentence_probs = []
+	word_probs = []
+	for sent in activation_maps:
+		word_probs.append(sent[0])
+		sentence_probs.append(sent[1])
+	dic['word_attention'] = word_probs
+	dic['sentence_attention'] = sentence_probs
+	return dic
+
+def train(dataset):
+	if dataset == "yelp":
+		(train_x, train_y), (test_x, test_y) = yelp.load_data(path=YELP_DATA_PATH, size=1e4, binary=False)
+		SAVED_MODEL_DIR = YELP_SAVED_MODEL_DIR
+	else:
+		(train_x, train_y), (test_x, test_y) = sentiment.load_data(path=SENTIMENT_DATA_PATH)
+		SAVED_MODEL_DIR = SENT_SAVED_MODEL_DIR
+
+	h = HNATT()
+	h.train(train_x, train_y,
+		batch_size=16,
+		epochs=16,
+		embeddings_path=None,
+		saved_model_dir=SAVED_MODEL_DIR,
+		saved_model_filename=SAVED_MODEL_FILENAME)
+
+def get_model(dataset):
+	h = HNATT()
+	if dataset == "yelp":
+		h.load_weights(YELP_SAVED_MODEL_DIR, SAVED_MODEL_FILENAME)
+	else:
+		h.load_weights(SENT_SAVED_MODEL_DIR, SAVED_MODEL_FILENAME)
+	return h
+
+
+
+if __name__ == '__main__':
+	# train("yelp")
+	# train("sentiment")
+	dataset = 'yelp'
+	h = get_model(dataset)
+	testcase = 'i agree that the seating is odd. but the food is exceptional especially for the price. the menu is truly montreal meats japan (spelling is correct) = very unique. great'
+	result = visual_info(h, testcase)
+	for key in result:
+	    print(key, result[key])
